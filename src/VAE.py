3ff4cb2ab6065cba6956b0970fea7d8a27f9af6a
coldtaco <36543008+coldtaco@users.noreply.github.com>
2020-12-24 15:03:04 +1100

    added testing in main

--- src/VAE.py
+++ src/VAE.py
@@ -49,10 +49,10 @@ class VAE(tf.keras.Model):
         super(VAE, self).__init__()
         
         if outlayersize == None:
-            outlayersize = reversed(inlayersize)
+            outlayersize = list(reversed(inlayersize))
 
         if outputsize == None:
-            outputsize = reversed(inputsize)
+            outputsize = list(reversed(inputsize))
 
         self.inlayers = []
         self.outlayers = []
@@ -72,11 +72,11 @@ class VAE(tf.keras.Model):
     def call(self, inp):
         for level in self.inlayers:
             inp = [layer(i) for layer, i in zip(level, inp)]
-
+        print(inp)
         inp = tf.concat(inp, 0)
         means, logvar = self.encoder(inp)
         
-        var = tf.math.exp(logvar)
+        var = tf.exp(0.5*logvar)
         
         norm = tf.random.normal([1], means, var)
 
@@ -93,33 +93,121 @@ def testloss(labels, predictions):
     e2 = tf.keras.losses.mean_squared_error(labels[1], predictions[1])
     return tf.math.reduce_mean([e1,e2])
 
-if __name__ == "__main__":
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from train import train, splitdata
-
+def test1():
+    matplotlib.get_backend()
     x1 = np.random.multivariate_normal([10,24], [[3,2],[2,3]], 100).astype('float32')
     x2 = np.random.multivariate_normal([-3,30], [[5,3],[3,6]], 100).astype('float32')
     x3 = np.random.multivariate_normal([-30,3], [[4,3],[3,4]], 100).astype('float32')
     x4 = np.array(list(zip(np.random.uniform(-50,50,10), np.random.uniform(-50,50,10)))).astype('float32')
     X = np.concatenate([x1, x2, x3, x4])
     Y = np.concatenate([np.zeros(len(x1)), np.zeros(len(x2)), np.zeros(len(x3)), np.ones(len(x4))])
+    plt.plot(X[Y==0,0], X[Y==0,1], '.b')
+    plt.plot(X[Y==1,0], X[Y==1,1], 'xb')
     Y = np.expand_dims(Y,-1).astype('float32')
     #print(list(zip(np.expand_dims(X,1),Y)))
 
-    vae = VAE([],[32,16,8], 4, outlayersize= [8,16,32],outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae = VAE([],[128,64,16], 8, outlayersize=[16,64,128], outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
     z = np.array(list(zip(X,Y)), dtype=object)
-    traindata, test = splitdata(X,z)
+    traindata, test = splitdata(X,z, seed = 1)
     trainin, trainout = traindata[:,0], traindata[:,1]
     testin, testout = test[:,0], test[:,1]
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    trainout = [np.stack(trainout[:,0]).astype('float32'),np.stack(trainout[:,1]).astype('float32')]
+    testout = [np.stack(testout[:,0]).astype('float32'),np.stack(testout[:,1]).astype('float32')]
 
     vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
-    vae.fit(trainin.astype('float32'), [np.stack(trainout[:,0]).astype('float32'),np.stack(trainout[:,1]).astype('float32')], epochs=500, batch_size=64, verbose = 2)
+    hist = vae.fit(trainin.astype('float32'), trainout, validation_data = (testin.astype('float32'), testout),epochs=2000, batch_size=64, verbose = 2)
     #train(vae, trainin.astype('float32'), [np.stack(trainout[:,0]).astype('float32'),np.stack(trainout[:,1]).astype('float32')], lossfunction = testloss)
-    pred = vae.predict(testin.astype('float32'))
-    pred = np.where(np.array(pred[1]) >= 0.5, 0, 1)
-    print(list(zip(np.stack(testout[:,1]),np.stack(pred))))
+
+
+
+    x1 = np.random.multivariate_normal([10,24], [[3,2],[2,3]], 100).astype('float32')
+    x2 = np.random.multivariate_normal([-3,30], [[5,3],[3,6]], 100).astype('float32')
+    x3 = np.random.multivariate_normal([-30,3], [[4,3],[3,4]], 100).astype('float32')
+    x4 = np.array(list(zip(np.random.uniform(-50,50,100), np.random.uniform(-50,50,100)))).astype('float32')
+    X = np.concatenate([x1, x2, x3, x4])
+    Y = np.concatenate([np.zeros(len(x1)), np.zeros(len(x2)), np.zeros(len(x3)), np.ones(len(x4))])
+
+    pred = vae.predict(X)
+    pred = np.where(np.array(pred[1]) >= 0.5, 1, 0)
     cm = np.zeros([2,2])
-    np.add.at(cm, [np.stack(testout[:,1]).astype('int'),pred], 1)
-    print(cm)
+    pred = np.squeeze(pred,1)
+    plt.plot(X[np.logical_and(Y==0, Y != pred),0], X[np.logical_and(Y==0, Y != pred),1], '.r')
+    plt.plot(X[np.logical_and(Y==1, Y != pred),0], X[np.logical_and(Y==1, Y != pred),1], 'xr')
+    plt.plot(X[np.logical_and(Y==0, Y == pred),0], X[np.logical_and(Y==0, Y == pred),1], '.k')
+    plt.plot(X[np.logical_and(Y==1, Y == pred),0], X[np.logical_and(Y==1, Y == pred),1], 'xk')
+
+    np.add.at(cm, (pred, Y.astype(int)), 1)
+    
+    vae.summary()
+    plt.show()
+
+def testdata(n1=100,n2=100,n3=100,n4=10):
+    x1 = np.random.multivariate_normal([10,24], [[3,2],[2,3]], n1).astype('float32')
+    x2 = np.random.multivariate_normal([-3,30], [[5,3],[3,6]], n2).astype('float32')
+    x3 = np.random.multivariate_normal([-30,3], [[4,3],[3,4]], n3).astype('float32')
+    x4 = np.array(list(zip(np.random.uniform(-50,50,10), np.random.uniform(-50,50,n4)))).astype('float32')
+    X = np.concatenate([x1, x2, x3, x4])
+    Y = np.concatenate([np.zeros(len(x1)), np.zeros(len(x2)), np.zeros(len(x3)), np.ones(len(x4))])
+    Y = np.expand_dims(Y,-1).astype('float32')
+    return X,Y
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from train import train, splitdata, crossvalidation, modeltests
+    import matplotlib
+    import random
+    random.seed(0)
+    np.random.seed(0)
+
+    X,Y = testdata()
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    
+    Xtest, Ytest = testdata(n4=100)
+
+
+    vae = VAE([],[128,64,16], 8, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '01', description='128|64|16, Latent of 8, 500 epochs against 100 from each distribution and 10 outliers')
+
+    vae = VAE([],[64,16], 8, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '02', description='64|16, Latent of 8, 500 epochs against 100 from each distribution and 10 outliers')
+
+    vae = VAE([],[64,16], 4, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '03', description='64|16, Latent of 4, 500 epochs against 100 from each distribution and 10 outliers')
+
+    vae = VAE([],[64,32], 16, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '04', description='64|16, Latent of 16 500 epochs against 100 from each distribution and 10 outliers')
+
+    vae = VAE([],[256,128,64], 16, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '05', description='256|128|64, Latent of 16, 500 epochs against 100 from each distribution and 10 outliers')
+
+
+    X,Y = testdata(200,200,200,20)
+
+
+    vae = VAE([],[128,64,16], 8, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '11', description='128|64|16, Latent of 8, 500 epochs against 200 from each distribution and 20 outliers')
+
+    vae = VAE([],[64,16], 8, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '12', description='64|16, Latent of 8, 500 epochs against 200 from each distribution and 20 outliers')
+
+    vae = VAE([],[64,16], 4, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '13', description='64|16, Latent of 4, 500 epochs against 200 from each distribution and 20 outliers')
+
+    vae = VAE([],[64,32], 16, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '14', description='64|16, Latent of 16 500 epochs against 200 from each distribution and 20 outliers')
+
+    vae = VAE([],[256,128,64], 16, outputsize = [[2,1]], finalactivation=[None, 'sigmoid'])
+    vae.compile(optimizer, loss=[tf.keras.losses.mean_squared_error, tf.keras.losses.binary_crossentropy])
+    modeltests(X,[X,Y], [Xtest, Ytest], model = vae, epochs = 500, name = '15', description='256|128|64, Latent of 16, 500 epochs against 200 from each distribution and 20 outliers')
