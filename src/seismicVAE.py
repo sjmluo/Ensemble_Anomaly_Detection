c81a5550ce8893f71163320220927f568f765779
coldtaco <36543008+coldtaco@users.noreply.github.com>
2020-12-30 19:50:19 +1100

    minor changes

--- src/seismicVAE.py
+++ src/seismicVAE.py
@@ -26,22 +26,51 @@ if __name__ == "__main__":
     from data_exploration.seismicdata import data
     from train import splitdata, modeltests
 
-    vae = VAE([[1,1,1,1,1,1,2,4,4,4,4],[4,4,4,4,4,4,8,16,16,16,16]], [256, 128, 64], 16, 
+    vae = VAE([[4,4,4,4,4,4,4,8,8,8,8],[8,8,8,8,8,8,16,32,32,32,32]], [516, 256, 128], 64, 
         outputsize=[[4,4,4,4,4,4,8,16,16,16,16,4], [1,1,1,1,1,1,2,4,4,4,4,1]],
         finalactivation=[None,None,None,None,None,None,None,None,None,None,None,'sigmoid'])
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-    losses = [tf.losses.mean_squared_error]*11
+    losses = [tf.losses.mean_absolute_error]*11
     losses.append(tf.losses.binary_crossentropy)
-    vae.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 5])
+    vae.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1])
     
+    earlystop = tf.keras.callbacks.EarlyStopping(
+    monitor='val_output_12_loss', min_delta=10e-7, patience=50, verbose=2,
+    mode='auto', baseline=None, restore_best_weights=True
+    )
+    plateau = tf.keras.callbacks.ReduceLROnPlateau()
+    logbar = tf.keras.callbacks.ProgbarLogger(stateful_metrics=['output_1_loss',
+        'output_2_loss',
+        'output_3_loss',
+        'output_4_loss',
+        'output_5_loss',
+        'output_6_loss',
+        'output_7_loss',
+        'output_8_loss',
+        'output_9_loss',
+        'output_10_loss',
+        'output_11_loss',
+        'val_output_1_loss',
+        'val_output_2_loss',
+        'val_output_3_loss',
+        'val_output_4_loss',
+        'val_output_5_loss',
+        'val_output_6_loss',
+        'val_output_7_loss',
+        'val_output_8_loss',
+        'val_output_9_loss',
+        'val_output_10_loss',
+        'val_output_11_loss','loss'])
+    #logbar.epochs = 25
+
     inp, tar = data()
 
-    '''traindata, test = splitdata(inp, tar, seed = 0)
-    trainin, trainout = traindata[:,0], traindata[:,1]
-    testin, testout = test[:,0], test[:,1]'''
-    modeltests(inp, tar, None, model = vae, epochs = 1, name = '03', 
-    description="""VAE([[1,1,1,1,1,1,2,4,4,4,4],[4,4,4,4,4,4,8,16,16,16,16]], [256, 128, 64], 16, 
+    modeltests(inp, tar, None, model = vae, epochs = 500, name = '04', 
+    description="""VAE([[4,4,4,4,4,4,4,8,8,8,8],[8,8,8,8,8,8,16,32,32,32,32]], [516, 256, 128], 64, 
         outputsize=[[4,4,4,4,4,4,8,16,16,16,16,4], [1,1,1,1,1,1,2,4,4,4,4,1]],
         finalactivation=[None,None,None,None,None,None,None,None,None,None,None,'sigmoid'])
-         500 epochs with bce with weighted losses: [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 5]"""
-        , dir = 'src/reports/seismic', verbose=2)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+        losses = [tf.losses.mean_absolute_error]*11
+        losses.append(tf.losses.binary_crossentropy)
+        vae.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 10]), using mae instead of mse"""
+        , dir = 'src/reports/seismic', verbose = 2,overall_kwargs = {'callbacks': [plateau,logbar]})
