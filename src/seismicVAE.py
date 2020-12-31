74c72150b34a6fd76b954d63f57a190b373371a1
coldtaco <36543008+coldtaco@users.noreply.github.com>
2020-12-31 12:02:19 +1100

    made changes to fit other changes

--- src/seismicVAE.py
+++ src/seismicVAE.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from VAE import VAE
 from train import train_step
+from datetime import datetime
 
 def lossfunction(labels, predictions):
     # Calculate loss across all labels then provide weighted loss towards the class predictor
@@ -22,6 +23,43 @@ def lossfunction(labels, predictions):
 
     return tf.nn.compute_average_loss(indivloss, sample_weight=weights)
 
+
+def callback():
+    plateau = tf.keras.callbacks.ReduceLROnPlateau()
+    logbar = tf.keras.callbacks.ProgbarLogger(stateful_metrics=['output_1_loss',
+        'output_2_loss',
+        'output_3_loss',
+        'output_4_loss',
+        'output_5_loss',
+        'output_6_loss',
+        'output_7_loss',
+        'output_8_loss',
+        'output_9_loss',
+        'output_10_loss',
+        'output_11_loss',
+        'val_output_1_loss',
+        'val_output_2_loss',
+        'val_output_3_loss',
+        'val_output_4_loss',
+        'val_output_5_loss',
+        'val_output_6_loss',
+        'val_output_7_loss',
+        'val_output_8_loss',
+        'val_output_9_loss',
+        'val_output_10_loss',
+        'val_output_11_loss','loss'])
+    logbar.epochs = 25
+    logdir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tb_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+
+    return [plateau,logbar, tb_callback]
+
+def compile_model(model):
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    losses = [tf.losses.mean_absolute_error]*11
+    losses.append(tf.losses.binary_crossentropy)
+    model.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1])
+
 if __name__ == "__main__":
     from data_exploration.seismicdata import data
     from train import splitdata, modeltests
@@ -29,10 +67,9 @@ if __name__ == "__main__":
     vae = VAE([[4,4,4,4,4,4,4,8,8,8,8],[8,8,8,8,8,8,16,32,32,32,32]], [516, 256, 128], 64, 
         outputsize=[[4,4,4,4,4,4,8,16,16,16,16,4], [1,1,1,1,1,1,2,4,4,4,4,1]],
         finalactivation=[None,None,None,None,None,None,None,None,None,None,None,'sigmoid'])
-    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-    losses = [tf.losses.mean_absolute_error]*11
-    losses.append(tf.losses.binary_crossentropy)
-    vae.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1])
+    
+    vae.addcompile(compile_model)
+    vae._compile()
     
     earlystop = tf.keras.callbacks.EarlyStopping(
     monitor='val_output_12_loss', min_delta=10e-7, patience=50, verbose=2,
@@ -61,16 +98,16 @@ if __name__ == "__main__":
         'val_output_9_loss',
         'val_output_10_loss',
         'val_output_11_loss','loss'])
-    #logbar.epochs = 25
+    logbar.epochs = 25
 
     inp, tar = data()
 
-    modeltests(inp, tar, None, model = vae, epochs = 500, name = '04', 
+    modeltests(inp, tar, None, model = vae, epochs = 150, name = '05', 
     description="""VAE([[4,4,4,4,4,4,4,8,8,8,8],[8,8,8,8,8,8,16,32,32,32,32]], [516, 256, 128], 64, 
         outputsize=[[4,4,4,4,4,4,8,16,16,16,16,4], [1,1,1,1,1,1,2,4,4,4,4,1]],
         finalactivation=[None,None,None,None,None,None,None,None,None,None,None,'sigmoid'])
         optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
         losses = [tf.losses.mean_absolute_error]*11
         losses.append(tf.losses.binary_crossentropy)
-        vae.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 10]), using mae instead of mse"""
-        , dir = 'src/reports/seismic', verbose = 2,overall_kwargs = {'callbacks': [plateau,logbar]})
+        vae.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 10]), using mae instead of mse, manually resetting callbacks"""
+        , dir = 'src/reports/seismic', verbose = 2,overall_kwargs = {'callbacks': callback})
