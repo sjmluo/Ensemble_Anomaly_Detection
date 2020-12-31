05405cee5cad0e2f8be0fe7ebb926c9b6d72729c
coldtaco <36543008+coldtaco@users.noreply.github.com>
2020-12-31 17:35:14 +1100

    changed callback function to allow for saving tensorboard output for different folds of CV in different folders

--- src/seismicVAE.py
+++ src/seismicVAE.py
@@ -24,8 +24,8 @@ def lossfunction(labels, predictions):
     return tf.nn.compute_average_loss(indivloss, sample_weight=weights)
 
 
-def callback():
-    plateau = tf.keras.callbacks.ReduceLROnPlateau()
+def callback(wdir):
+    plateau = tf.keras.callbacks.ReduceLROnPlateau(verbose=1)
     logbar = tf.keras.callbacks.ProgbarLogger(stateful_metrics=['output_1_loss',
         'output_2_loss',
         'output_3_loss',
@@ -49,16 +49,27 @@ def callback():
         'val_output_10_loss',
         'val_output_11_loss','loss'])
     logbar.epochs = 25
-    logdir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-    tb_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+    #logdir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tb_callback = tf.keras.callbacks.TensorBoard(log_dir=wdir)
 
-    return [plateau,logbar, tb_callback]
+    earlystop = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss', min_delta=10e-7, patience=18, verbose=2,
+    mode='auto', baseline=None, restore_best_weights=True
+    )
+
+    return [plateau,logbar, tb_callback, earlystop]
 
 def compile_model(model):
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
     losses = [tf.losses.mean_absolute_error]*11
-    losses.append(tf.losses.binary_crossentropy)
-    model.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1])
+    #losses = []
+    from train import weightedce
+    from functools import partial
+    ce = partial(weightedce, weights = tf.constant([[1,10],[1,1]], dtype = tf.float32))
+    #losses.append(tf.losses.binary_crossentropy)
+    losses.append(ce)
+    ce.__name__ = 'weightedce'
+    model.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 10])
 
 if __name__ == "__main__":
     from data_exploration.seismicdata import data
@@ -70,44 +81,14 @@ if __name__ == "__main__":
     
     vae.addcompile(compile_model)
     vae._compile()
-    
-    earlystop = tf.keras.callbacks.EarlyStopping(
-    monitor='val_output_12_loss', min_delta=10e-7, patience=50, verbose=2,
-    mode='auto', baseline=None, restore_best_weights=True
-    )
-    plateau = tf.keras.callbacks.ReduceLROnPlateau()
-    logbar = tf.keras.callbacks.ProgbarLogger(stateful_metrics=['output_1_loss',
-        'output_2_loss',
-        'output_3_loss',
-        'output_4_loss',
-        'output_5_loss',
-        'output_6_loss',
-        'output_7_loss',
-        'output_8_loss',
-        'output_9_loss',
-        'output_10_loss',
-        'output_11_loss',
-        'val_output_1_loss',
-        'val_output_2_loss',
-        'val_output_3_loss',
-        'val_output_4_loss',
-        'val_output_5_loss',
-        'val_output_6_loss',
-        'val_output_7_loss',
-        'val_output_8_loss',
-        'val_output_9_loss',
-        'val_output_10_loss',
-        'val_output_11_loss','loss'])
-    logbar.epochs = 25
 
     inp, tar = data()
+    #tar = [tar[-1]]
 
-    modeltests(inp, tar, None, model = vae, epochs = 150, name = '05', 
+    modeltests(inp, tar, None, model = vae, epochs = 500, name = '14', k=10,
     description="""VAE([[4,4,4,4,4,4,4,8,8,8,8],[8,8,8,8,8,8,16,32,32,32,32]], [516, 256, 128], 64, 
         outputsize=[[4,4,4,4,4,4,8,16,16,16,16,4], [1,1,1,1,1,1,2,4,4,4,4,1]],
         finalactivation=[None,None,None,None,None,None,None,None,None,None,None,'sigmoid'])
-        optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-        losses = [tf.losses.mean_absolute_error]*11
-        losses.append(tf.losses.binary_crossentropy)
-        vae.compile(optimizer, loss=losses, loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 10]), using mae instead of mse, manually resetting callbacks"""
-        , dir = 'src/reports/seismic', verbose = 2,overall_kwargs = {'callbacks': callback})
+         with weightedce[[1,10],[1,1]],
+         loss_weights=[1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 10], 10fold cv"""
+        , wdir = 'src/reports/seismic', verbose = 2,overall_kwargs = {'callbacks': callback})
