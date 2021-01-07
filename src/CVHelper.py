f3eca96abd180aaa853ff4c93d637faac94ba2c8
coldtaco <36543008+coldtaco@users.noreply.github.com>
2021-01-07 16:32:37 +1100

    added a few more metrics, improved save checking

--- src/CVHelper.py
+++ src/CVHelper.py
@@ -101,10 +101,14 @@ class CVHelper:
 
     def saveCheck(self, incattr):
         if incattr['description'] != self.description:
+            print(f'Description is different {incattr["description"]}, {self.description}')
             return False
         if incattr['data'] != self.data:
+            print('Data is different')
             return False
         if incattr['compile_func'] != self.compile_func:
+            print(f'Compile function is different {incattr["compile_func"]}, {self.compile_func}')
+            print(f'Compile function is different {incattr["compile_func"].__code__.co_code}, {self.compile_func.__code__.co_code}')
             return False
         return True
 
@@ -149,7 +153,7 @@ class CVHelper:
             fold_ind = kfoldstratify(self.labels.iloc[:,-1], self.k)
 
             if self.progress[1] == 0:
-                self.results[f'run{i}'] = {'acc':[], 'loss':[], 'cm':[], 'spec':[], 
+                self.results[f'run{i}'] = {'acc':[], 'loss':[], 'cm':[], 'spec':[], 'y_pred': [], 'y_true': [], 'loglikelihood': [],
                     'totalTime':0, 'trainingTime':0}
             results = self.results[f'run{i}']
 
@@ -212,8 +216,10 @@ class CVHelper:
             f"Average accuracy: {ave}", 
             f"Balanced acc: {(sensi+spec)/2}", 
             f"Average specificity: {spec}", 
-            f"Average sensitivity: {sensi}",
+            f"Average sensitivity (Detection rate): {sensi}",
             f"Average loss: {loss}", 
+            f"Average False Alarm: {a[0,1]/a[:,1].sum()}", 
+            f"Average F1: {2*a[1,1]/(2*a[1,1]+a[0,1],a[1,0])}", 
             f"Average cm:", 
             f"||True 0| True 1|\n|-|-|-|\n|Predicted 0|{a[0][0]}|{a[0][1]}\n|Predicted 1|{a[1][0]}|{a[1][1]}\n", 
             f"|Acc|Spec|Loss|\n{ave}|{spec}|{loss}",
@@ -232,9 +238,13 @@ class CVHelper:
         self.trainingTime += time.perf_counter() - start
 
         pred = self.model.predict(testin)
+        results['y_pred'] = np.concatenate([results['y_pred'], np.squeeze(pred[-1], -1)])
+        results['y_true'] = np.concatenate([results['y_true'], np.squeeze(testout[-1], -1)])
         cm = confusionmat(pred[-1], testout[-1])
         results['loss'].append([hist[l][-1] for l in self.loss_record])
         results['acc'].append((cm[0][0] + cm[1][1])/cm.sum())
         results['spec'].append(cm[1][1]/(cm[:,1].sum()))
         results['cm'].append(cm)
-
+        sq = np.squeeze(testout[-1], -1).astype('int32')
+        pred = np.squeeze(pred[-1], -1)
+        results['loglikelihood'].append(np.log(pred[sq == 0]).sum() + np.log(1 - pred[sq == 1]).sum())
