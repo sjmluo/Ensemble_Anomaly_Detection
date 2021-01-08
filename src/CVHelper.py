4a9d498fc2f67cf952ce9fe3b38f37cbd73e6b7e
coldtaco <36543008+coldtaco@users.noreply.github.com>
2021-01-08 13:37:31 +1100

    added better documentation, added more custom logging features

--- src/CVHelper.py
+++ src/CVHelper.py
@@ -50,7 +50,7 @@ def set_seed(seed):
     os.environ['PYTHONHASHSEED'] = str(seed)
 
 class CVHelper:
-    def __init__(self, vaeArgs, data, compile_func, name, cvRuns = 3,
+    def __init__(self, modelArgs, data, compile_func, name, cvRuns = 3,
                 description = None, 
                 epochs=500, 
                 k = 10, 
@@ -61,6 +61,22 @@ class CVHelper:
                 train_preprocessing = None,
                 test_preprocessing = None,
                 **kwargs):
+        """
+        kwargs:
+        model: Class of model to be created
+        postfold: Function with signature postfold(model, result), called after fitting on a fold
+        writeResults: Function with signature writeResults(file, result), logs calculated metrics into file
+        postresult: Function with signature postresult(file, result), allows for writing results to file.
+         Similar to write results but called on results form postfold
+        callbacks: function that returns callbacks
+        """
+
+        valid_kwargs = {'model', 'postfold', 'postresult','callbacks', 'writeresults'}
+        
+        for kw in kwargs:
+            if kw not in valid_kwargs:
+                raise ValueError(f'{kw} is not a valid kwarg')
+
         wdir = f'{wdir}/{name}'
 
         self.save = Save(wdir)
@@ -77,10 +93,14 @@ class CVHelper:
             self.loadArgs()
             return
 
-        self.vaeArgs = vaeArgs
+        self.model_class = VAE if 'model' not in kwargs else kwargs['model']
+        self.modelArgs = modelArgs
 
         self.resetData()
 
+        self.postfold = None if 'postfold' not in kwargs else kwargs['postfold']
+        self.postresult = None if 'postresult' not in kwargs else kwargs['postresult']
+        self._writeResults = writeResults if 'writeResults' not in kwargs else kwargs['writeResults']
         self.epochs = epochs
         self.k = k
         self.cvRuns = cvRuns
@@ -135,7 +155,7 @@ class CVHelper:
         self.resetData()
     
     def createModel(self):
-        self.model = VAE(**self.vaeArgs)
+        self.model = self.model_class(**self.modelArgs)
         self.model.addcompile(self.compile_func)
         self.model._compile()
     
@@ -156,6 +176,9 @@ class CVHelper:
                 self.results[f'run{i}'] = {'acc':[], 'loss':[], 'cm':[], 'spec':[], 'y_pred': [], 'y_true': [], 'loglikelihood': [],
                     'totalTime':0, 'trainingTime':0}
             results = self.results[f'run{i}']
+
+            if self.postfold is not None:
+                results['post'] = {}
 
             results['totalTime'] += time.perf_counter() - start
 
@@ -181,20 +204,21 @@ class CVHelper:
                 
                 self.save.commit(self.__dict__)
 
+            self.resetData()
+
             with open(f'{self.wdir}/run{self.progress[0]}/results', 'w') as file:
                 self.writeResults(file, results)
 
             self.progress[0] += 1
             self.progress[1] = 0
 
-            self.totalTime = 0
             self.trainingTime = 0
         
         with open(f'{self.wdir}/overall', 'w') as file:
-            VAEargs = ['inputsize', 'inlayersize', 'latentsize', 'outlayersize', 'outputsize', 'finalactivation']
-            for a in VAEargs:
-                file.write(f'{a}\n{self.model.__dict__[a]}\n')
-            results = {'acc':[], 'loss':[], 'cm':[], 'spec':[], 
+            inf = self.model.info()
+            for a in inf:
+                file.write(f'{a}: {inf[a]}\n')
+            results = {'acc':[], 'loss':[], 'cm':[], 'spec':[], 'loglikelihood': [],
                     'totalTime':0, 'trainingTime':0}
             for key in results:
                 lst = [self.results[run][key] for run in self.results]
@@ -205,28 +229,13 @@ class CVHelper:
         return results
 
     def writeResults(self, file, results):
-        a = np.mean(results['cm'],axis = 0)
-        ave = (a[0][0] + a[1][1])/a.sum()
-        spec = a[1][1]/(a[:,1].sum())
-        loss = np.mean(results['loss'],0)
-        sensi = a[0][0]/a[:,0].sum()
         if self.description is not None:
             file.write(f'{self.description}\n')
-        file.writelines('\n'.join([
-            f"Average accuracy: {ave}", 
-            f"Balanced acc: {(sensi+spec)/2}", 
-            f"Average specificity: {spec}", 
-            f"Average sensitivity (Detection rate): {sensi}",
-            f"Average loss: {loss}", 
-            f"Average False Alarm: {a[0,1]/a[:,1].sum()}", 
-            f"Average F1: {2*a[1,1]/(2*a[1,1]+a[0,1],a[1,0])}", 
-            f"Average cm:", 
-            f"||True 0| True 1|\n|-|-|-|\n|Predicted 0|{a[0][0]}|{a[0][1]}\n|Predicted 1|{a[1][0]}|{a[1][1]}\n", 
-            f"|Acc|Spec|Loss|\n{ave}|{spec}|{loss}",
-            f"CV took {results['totalTime']} seconds",
+        self._writeResults(file, results)
+        file.write('\n'.join([f"CV took {results['totalTime']} seconds",
             f"Pure training took {results['trainingTime']}"]))
-
-
+        if self.postresult is not None:
+            self.postresult(file, results['post'])
 
     def fold(self, trainin, trainout, testin, testout, results):
         default_kwargs = {'x':trainin, 'y':trainout, 'epochs': self.epochs, 'verbose': self.verbose, 'validation_data':(testin, testout)}
@@ -238,13 +247,39 @@ class CVHelper:
         self.trainingTime += time.perf_counter() - start
 
         pred = self.model.predict(testin)
-        results['y_pred'] = np.concatenate([results['y_pred'], np.squeeze(pred[-1], -1)])
-        results['y_true'] = np.concatenate([results['y_true'], np.squeeze(testout[-1], -1)])
-        cm = confusionmat(pred[-1], testout[-1])
+        
+        crunch_predictions(pred[-1], testout[-1], results)
+
         results['loss'].append([hist[l][-1] for l in self.loss_record])
-        results['acc'].append((cm[0][0] + cm[1][1])/cm.sum())
-        results['spec'].append(cm[1][1]/(cm[:,1].sum()))
-        results['cm'].append(cm)
-        sq = np.squeeze(testout[-1], -1).astype('int32')
-        pred = np.squeeze(pred[-1], -1)
-        results['loglikelihood'].append(np.log(pred[sq == 0]).sum() + np.log(1 - pred[sq == 1]).sum())
+
+        if self.postfold is not None: self.postfold(self.model, results['post'])
+
+
+def writeResults(file, results):
+    a = np.mean(results['cm'],axis = 0)
+    ave = (a[0][0] + a[1][1])/a.sum()
+    spec = a[1][1]/(a[:,1].sum())
+    loss = np.mean(results['loss'],0)
+    sensi = a[0][0]/a[:,0].sum()
+    file.writelines('\n'.join([
+        f"Average accuracy: {ave}", 
+        f"Balanced acc: {(sensi+spec)/2}", 
+        f"Average specificity: {spec}", 
+        f"Average sensitivity (Detection rate): {sensi}",
+        f"Average loss: {loss}", 
+        f"Average False Alarm: {a[0,1]/a[:,1].sum()}", 
+        f"Average F1: {2*a[1,1]/(2*a[1,1]+a[0,1],a[1,0])}", 
+        f"Average cm:", 
+        f"||True 0| True 1|\n|-|-|-|\n|Predicted 0|{a[0][0]}|{a[0][1]}\n|Predicted 1|{a[1][0]}|{a[1][1]}\n", 
+        f"|Acc|Spec|Loss|\n{ave}|{spec}|{loss}"]))
+
+def crunch_predictions(y_pred, y_true, results):
+    results['y_pred'] = np.concatenate([results['y_pred'], np.squeeze(y_pred, -1)])
+    results['y_true'] = np.concatenate([results['y_true'], np.squeeze(y_true, -1)])
+    cm = confusionmat(y_pred, y_true)
+    results['acc'].append((cm[0][0] + cm[1][1])/cm.sum())
+    results['spec'].append(cm[1][1]/(cm[:,1].sum()))
+    results['cm'].append(cm)
+    sq = np.squeeze(y_true, -1).astype('int32')
+    pred = np.squeeze(y_pred, -1)
+    results['loglikelihood'].append(np.log(pred[sq == 0]).sum() + np.log(1 - pred[sq == 1]).sum())
